7064f0dd5f0770218c7360f6a89fb10f4ce9a9ac
Juan Pablo Arce <juanjprot@gmail.com>
2022-09-04 00:18:54 -0300

    Neater test printing

--- Clafac.Throwaway/Program.fs
+++ Clafac.Throwaway/Program.fs
@@ -3,7 +3,7 @@ open Clafac.NumberRepresentation.FloatImplementations
 open Clafac.NumberRepresentation.SoftwareFloat
 
 let myOnePointFive = Ieee754Single.Float(false, 0, 0b11000000_00000000_00000000UL)
-let myDenormal = Ieee754Single.Float(false, -126, 0b1000000_00000000_00000000UL)
+let myDenormal = Ieee754Single.Float(false, -126, 0b01000000_00000000_00000000UL)
 
 let makeIeee f = Ieee754Single.Float(f.Sign, f.Exponent, f.Mantissa)
 
@@ -13,11 +13,15 @@ let test (myInitial: Ieee754Single.Float) =
     let myRestored = makeIeee (subtract myTripled.Value myDoubled.Value)
     let myZeroed = makeIeee (subtract myTripled.Value myTripled.Value)
 
-    printf $"%s{myInitial.HardwareValue.ToString()}\t%d{myInitial.Value.Exponent}\n"
-    printf $"%s{myDoubled.HardwareValue.ToString()}\t%d{myDoubled.Value.Exponent}\n"
-    printf $"%s{myTripled.HardwareValue.ToString()}\t%d{myTripled.Value.Exponent}\n"
-    printf $"%s{myRestored.HardwareValue.ToString()}\t%d{myRestored.Value.Exponent}\n"
-    printf $"%s{myZeroed.HardwareValue.ToString()}\t%d{myZeroed.Value.Exponent}\n"
+    let printStats (f: Ieee754Single.Float) =
+        printf $"%s{f.HardwareValue.ToString()}\t%d{f.Value.Exponent}\t{f.ExponentRepresentation}\n"
+    
+    printStats myInitial
+    printStats myDoubled
+    printStats myTripled
+    printStats myRestored
+    printStats myZeroed
+
     printf "**************************\n"
 
 test myDenormal
